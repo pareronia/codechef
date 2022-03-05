b91d7fd0fa20135fd5fc234846b00cb6a752edf3
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-05 19:42:18 +0100

    ALTERNATEDIV

---
+++ src/main/java/com/github/pareronia/codechef/alternatediv/AlternatingDivisibility.java
@@ -0,0 +1,160 @@
+package com.github.pareronia.codechef.alternatediv;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * Alternating Divisibility
+ * @see <a href="https://www.codechef.com/COOK139C/problems/ALTERNATEDIV">https://www.codechef.com/COOK139C/problems/ALTERNATEDIV</a>
+ */
+class AlternatingDivisibility {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public AlternatingDivisibility(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final int[] ans = new int[n];
+        final Set<Integer> seen = new HashSet<>();
+        ans[0] = 1;
+        seen.add(1);
+        if (n > 1) {
+            ans[1] = 3;
+            seen.add(3);
+            for (int j = 2; j < n; j++) {
+                if (j % 2 == 0) {
+                    int k = 1;
+                    while (seen.contains(ans[j - 2] + k)) {
+                        k++;
+                    }
+                    ans[j] = ans[j - 2] + k;
+                } else {
+                    ans[j] = ans[j - 1] * 2;
+                }
+                assert ans[j] <= 2 * n;
+                seen.add(ans[j]);
+            }
+        }
+        this.out.println(Arrays.stream(ans)
+                .mapToObj(String::valueOf).collect(joining(" ")));
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases = sc.nextInt();
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = AlternatingDivisibility.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new AlternatingDivisibility(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(AlternatingDivisibility.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("codechef"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
